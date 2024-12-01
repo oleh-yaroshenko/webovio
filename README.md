1bcb92165e09785c3f5997f870394b0b63dc9c99
Oleh Yaroshenko <oleg21062010@gmail.com>
2024-12-01 21:37:12 +0000

    Formatted code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Webovio
--
+## Webovio
+
 **Notion:** https://www.notion.so/Webovio-192cd6a93ce64254931f8d7d3a3cf559?pvs=4
 
 **Figma:** https://www.figma.com/design/XuFL63tgK7bTSMrdwv308n/Webovio?node-id=0-2&node-type=frame&t=81FkQgskFejrNXZp-0
--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const menu = document.querySelector(".header-menu")
 const showMenu = document.querySelector(".header-nav-burger")
 const hideMenu = document.querySelector(".header-menu-top-burger")
 
-showMenu.addEventListener("click", function() {
+showMenu.addEventListener("click", function () {
     menu.classList.add("header-menu-show")
 })
 
-hideMenu.addEventListener("click", function() {
+hideMenu.addEventListener("click", function () {
     menu.classList.remove("header-menu-show")
 })
